28ac6cc0bf9b77a7405c755078bbb1768e51f287
Gini <64064123+gin4eto7@users.noreply.github.com>
2020-10-31 11:18:34 +0200

    GenericsLab

---
+++ GenericArrayCreator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] array = ArrayCreator.Create(10, "Pesho");
+            Console.WriteLine(String.Join(",", array));
+            int[] integers = ArrayCreator.Create(10, 33);
+            Console.WriteLine(String.Join(",", integers));
+        }
+    }
+}
